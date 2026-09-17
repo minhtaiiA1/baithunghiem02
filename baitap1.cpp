36e14ad45ae93bf0cd48aa39b502764f658db3ac
minhtaiiA1 <taib2613078@student.ctu.edu.vn>
2026-09-17 11:52:19 +0700

    C++ dau tien

---
+++ baitap1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	int a;
+	cout<<"thi tot nghiep duoc bao nhieu diem:";
+	cin >>a;
+	if(a>=15){
+		cout<<"very good";
+	} else{
+		cout<<"cook";
+	}
+	return 0;
+}
